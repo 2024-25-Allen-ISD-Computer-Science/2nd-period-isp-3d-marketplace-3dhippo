e2f54427e387f4160ebe30db1cf25d3f15bdcf35
Ron Shaked <ronshaked07@gmail.com>
2025-01-16 16:13:50 -0600

    Moved from local db to atlas_db

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -1,7 +1,10 @@
 import { MongoClient } from "mongodb";
 
-// Define the URI and database name
-const uri = "mongodb://localhost:27017/3dhippo";
+// Define the MongoDB Atlas URI
+const uri = process.env.MONGODB_URI || "mongodb+srv://ronshaked07:<3o7ZX28kXbR7Laik>@aeris.xaqqj.mongodb.net/?retryWrites=true&w=majority&appName=Aeris";
+
+// Define the database name
+const dbName = "3dhippo";
 
 // Define a variable to store the MongoClient instance
 let client: MongoClient | null = null;
@@ -15,12 +18,12 @@ export async function getDbClient(): Promise<MongoClient> {
     try {
       // Connect to MongoDB
       await client.connect();
-      console.log("Connected to MongoDB");
+      console.log(`Connected to MongoDB database: ${dbName}`);
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
       throw error;
     }
   }
-  
+
   return client;
 }
